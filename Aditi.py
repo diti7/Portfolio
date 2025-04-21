e3e9bf06eebc7dbf88c02dbb1bd3f1aeeeb3a7cb
Aditi <aditipatel755@gmail.com>
2025-04-20 22:19:13 -0400

    Update Aditi.py

--- Aditi.py
+++ Aditi.py
@@ -64,8 +64,7 @@ if page == "home":
 
     #Portrait...
     with col3:
-        #st.image("images/portrait.jpg", width = 265)
-        st.image("https://github.com/diti7/Portfolio/blob/main/images/Portrait.jpg", width=265)
+        st.image("images/portrait.jpg", width = 265)
 
 
     #Tech Stack...

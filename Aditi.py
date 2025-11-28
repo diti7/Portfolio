9e5ea055d6ceb645c6c295e8d502a1f4a809f9a5
Aditi <aditipatel755@gmail.com>
2025-11-27 20:26:37 -0500

    Update Aditi.py

--- Aditi.py
+++ Aditi.py
@@ -106,7 +106,7 @@ if page == "home":
     }"""
     skill_col_size = 5
     tech = {
-        , st.header("🛠 Tech Stack")
+        st.header("🛠 Tech Stack")
         col1, col2, col3 = st.columns(3)
 
     with col1:
@@ -241,6 +241,7 @@ with st.expander("📌 Current Portfolio Summary"):
     """)
 
 st.info("⭐ More content will be updated continuously during co-op.")
+
 
 
 
